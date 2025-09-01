2986488ddec8e3589199d775af16e4ccceb1c195
mschary1994 <mschary7@gmail.com>
2025-09-01 21:07:02 +0530

    Update cors.js

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,11 +1,23 @@
 const cors = require("cors");
 const { env } = require("./env");
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  env.UI_URL   // e.g. http://135.235.192.34:5173
+];
+
 const corsPolicy = cors({
-  origin: env.UI_URL,
-  method: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Accept", "Origin", "X-CSRF-TOKEN"],
+  origin: function (origin, callback) {
+    // allow requests with no origin (like curl, Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS: " + origin));
+    }
+  },
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Accept", "Origin", "X-CSRF-TOKEN"],
 });
 
 module.exports = { corsPolicy };
